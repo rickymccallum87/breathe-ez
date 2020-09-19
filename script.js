4178b7fa3434e521d66397cabb06d274afd8f11c
Ricky McCallum <contact@rickymccallum.com>
2020-09-19 10:29:30 -0400

    Breathe in once on page load

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var breathBar = document.querySelector('.breath-bar');
 
 // add .breathe-in
+breathBar.classList.add('breathe-in');
 // when transition completes, remove .breathe-in and add .breathe-out
 // repeat

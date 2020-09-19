9a41435c9af24749fd77a4943603539b5ffee458
Ricky McCallum <contact@rickymccallum.com>
2020-09-19 10:40:40 -0400

    Loop breathing in and out

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var breathBar = document.querySelector('.breath-bar');
-
-// add .breathe-in
 breathBar.classList.add('breathe-in');
-// when transition completes, remove .breathe-in and add .breathe-out
-// repeat
+
+let transitionEndEventName = getTransitionEndEventName();
+breathBar.addEventListener(transitionEndEventName, function () {
+  breathBar.classList.toggle('breathe-in');
+  breathBar.classList.toggle('breathe-out');
+})
+
+function getTransitionEndEventName() {
+  var transitions = {
+    "transition": "transitionend",
+    "OTransition": "oTransitionEnd",
+    "MozTransition": "transitionend",
+    "WebkitTransition": "webkitTransitionEnd"
+  }
+  let bodyStyle = document.body.style;
+  for (let transition in transitions) {
+    if (bodyStyle[transition] != undefined) {
+      return transitions[transition];
+    }
+  }
+}
